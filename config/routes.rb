6449ef254b8a2d27e36625c67b8523f9a598108c
Abao01 <2519663066h@gmail.com>
2022-03-31 15:33:58 -0500

    Modified the games and publishers controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'publishers/index'
+  get 'publishers/show'
+  get 'games/index'
+  get 'games/show'
+
+  resource :games, only: [:index, :show]
+  resource :publishers, only: [:index, :show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
