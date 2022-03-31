d330ebeec7b329e70e6db399f0d0ca6074e12dc1
Abao01 <2519663066h@gmail.com>
2022-03-30 22:20:46 -0500

    Added Game and Publisher models along with associations and validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_024541) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_030252) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -37,4 +37,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_024541) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.string "role"
+    t.integer "publisher_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["publisher_id"], name: "index_games_on_publisher_id"
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "games", "publishers"
 end
